d095a0386ca237db3f06e83bc33c561c0977d8c4
Christopher Johnson <cjhawk7@hotmail.com>
2018-02-14 19:21:14 -0700

    first commit

---
+++ test/tests-for-recipes.js
@@ -0,0 +1,89 @@
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+
+const {app, closeServer, runServer} = require('../server');
+
+const expect = chai.expect;
+
+chai.use(chaiHttp);
+
+let server;
+
+describe('recipes', function() {
+
+
+    before(function() {
+        return runServer();
+    });
+
+    after(function() {
+        return closeServer();
+    });
+
+    it('should list recipes on GET', function() {
+      return chai.request(app)
+        .get('/recipes')
+        .then(function(res) {
+          expect(res).to.have.status(200);
+          expect(res).to.be.json;
+          expect(res.body).to.be.a('array');
+  
+          expect(res.body.length).to.be.at.least(1);
+
+          const expectedKeys = ['id','name', 'ingredients'];
+          res.body.forEach(function(item) {
+            expect(item).to.be.a('object');
+            expect(item).to.include.keys(expectedKeys);
+          });
+        });
+    });
+});
+
+it('should add a recipe on POST', function() {
+    const newItem = {name: 'coffee', ingredient: ['coffee beans']};
+    return chai.request(app)
+      .post('/recipes')
+      .send(newItem)
+      .then(function(res) {
+        expect(res).to.have.status(201);
+        expect(res).to.be.json;
+        expect(res.body).to.be.a('object');
+        expect(res.body).to.include.keys('id', 'name', 'ingredients');
+        expect(res.body.id).to.not.equal(null);
+        expect(res.body).to.deep.equal(Object.assign(newItem, {id: res.body.id}));
+    });
+});
+
+it('should update recipes on PUT', function() {
+    const updateData = {
+      name: 'foo',
+      ingredient: 'bar'
+    };
+    return chai.request(app)
+      // first have to get so we have an idea of object to update
+      .get('/recipes')
+      .then(function(res) {
+        updateData.id = res.body[0].id;
+        return chai.request(app)
+          .put(`/recipes/${updateData.id}`)
+          .send(updateData)
+      })
+      .then(function(res) {
+        expect(res).to.have.status(200);
+        expect(res).to.be.json;
+        expect(res.body).to.be.a('object');
+        expect(res.body).to.deep.equal(updateData);
+      });
+});
+
+it('should delete recipes on DELETE', function() {
+    return chai.request(app)
+      .get('/recipes')
+      .then(function(res) {
+        return chai.request(app)
+          .delete(`/recipes/${res.body[0].id}`);
+    })
+      .then(function(res) {
+        expect(res).to.have.status(204);
+      });
+});
